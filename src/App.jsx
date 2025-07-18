982fdfa28510e46921e358acc12fd3b281e1ee4b
Anna Paulette Cambron Barrales <48526856+paulettecb@users.noreply.github.com>
2025-07-17 19:34:19 -0600

    Add dark mode with theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Home from './views/Home'
 import ChatbotWidget from './components/ChatbotWidget'
+import ThemeToggle from './components/ThemeToggle'
 
 function App() {
   return (
     <>
       <Home />
       <ChatbotWidget />
+      <ThemeToggle />
     </>
   )
 }
