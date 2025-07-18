bf34845856d615c26cdd7968e44949f8772e3a47
Anna Paulette Cambron Barrales <48526856+paulettecb@users.noreply.github.com>
2025-07-18 09:45:01 -0600

    feat: add properties page (#10)

---
+++ src/views/Properties.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropertyList from '../components/PropertyList'
+
+const Properties = () => (
+  <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+    <h1>Propiedades disponibles</h1>
+    <PropertyList />
+  </main>
+)
+
+export default Properties
